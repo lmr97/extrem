29332a179eb7c646080393a610b6df84ebeaa3e1
Martin Reid <lmreid1997@gmail.com>
2024-11-15 23:44:31 -0700

    reformatted, incl. checks for path validity

--- utils/config_ext_drive.py
+++ utils/config_ext_drive.py
@@ -4,19 +4,44 @@ import json
 with open("external_drive_paths.json", "r") as paths_file:
     drive_paths = json.load(paths_file)
 
+print("It looks like you haven't selected an external drive yet.")
+
+prompt  = """
+    Please enter the (absolute) filepath to your external hard drive,
+    using %s to separate folder names.
+    (this path will be saved for latter, so you won't have to enter it again.)
+
+    To find the filepath to your drive: 
+        1. Open File Explorer, and navigate to your external hard drive.
+        2. In the bar near the top of the window, right-click your drive's name.
+        3. In the right-click menu, select "Copy Address".
+        4. Paste the address below!
+"""
+
+
 dos_like  = "backslashes (\\)"
 unix_like = "forward slashes (/)"
-prompt  = "Please enter the absolute filepath to your external hard drive, \n"
 
-# if it's a Windows operating system
-if (os.name == "nt"):
-    prompt += "using " + dos_like + " to separate folders.\n"
-    path = input(prompt)
-    drive_paths["DOSFilePath"] = path
-else:
-    prompt += "using " + unix_like + " to separate folders.\n\n"
-    path = input(prompt)
-    drive_paths["UnixFilePath"] = path
+filepath_is_valid = False
+
+while(not filepath_is_valid):
+
+    # if it's a Windows operating system
+    if (os.name == "nt"):
+        path = input(prompt % dos_like)
+        drive_paths["DOSFilePath"] = path
+    else:
+        path = input(prompt % unix_like)
+        drive_paths["UnixFilePath"] = path
+    
+    filepath_is_valid = os.path.exists(path)
+
+    if (not filepath_is_valid):
+        print("That path does not lead to an external drive.")
+        print("Make sure to check the spelling, and try again!")
+
+
+print("\nThanks! If you use a different drive, please let me know.")
 
 with open("external_drive_paths.json", "w") as paths_file:
     json.dump(drive_paths, paths_file, indent=4)
