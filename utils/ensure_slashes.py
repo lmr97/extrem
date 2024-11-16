8287f0c37349118237691bc0877ac715fcede908
Martin Reid <lmreid1997@gmail.com>
2024-11-15 23:44:48 -0700

    actually writes changes now

--- utils/ensure_slashes.py
+++ utils/ensure_slashes.py
@@ -5,8 +5,17 @@ import json
 with open("external_drive_paths.json", "r") as paths_file:
     drive_paths = json.load(paths_file)
 
-if (drive_paths["UnixFilePath"][-1] != "/"):
-    drive_paths["UnixFilePath"] += "/"
+if (drive_paths["UnixFilePath"]):
 
-if (drive_paths["DOSFilePath"][-1] != "\\"):
-    drive_paths["DOSFilePath"] += "\\"
+    if (drive_paths["UnixFilePath"][-1] != "/"):
+        drive_paths["UnixFilePath"] += "/"
+
+if (drive_paths["DOSFilePath"]):
+
+    if (drive_paths["DOSFilePath"][-1] != "\\"):
+        drive_paths["DOSFilePath"] += "\\"
+
+# else, do nothing
+
+with open("external_drive_paths.json", "w") as paths_file:
+    json.dump(drive_paths, paths_file, indent=4)
