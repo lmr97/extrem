d6a4e62778a2b5ebd54d40a1e671f8bc8dd8cd61
Martin Reid <lmreid1997@gmail.com>
2024-11-16 20:07:26 -0700

    took out ID saves/lookups, added automatic MIME types

--- save_to_google_drive.py
+++ save_to_google_drive.py
@@ -2,6 +2,7 @@ import os.path
 os.system("")   # makes colors process right for Windows
 import sys
 import json
+import mimetypes
 from json.decoder import JSONDecoder
 
 from google.auth.transport.requests import Request
@@ -16,38 +17,27 @@ SCOPES = ["https://www.googleapis.com/auth/drive"]
 
 
 class FileToUpload:
-    def __init__(self, filename, dest_Drive_folder=None):
+    def __init__(self, filepath, dest_Drive_folder=None):
 
-        if (not filename or filename != ""): 
+        if (not filepath or filepath == ""): 
             raise FileNotFoundError("I need a filename!")
 
-        self.filename    = filename
+        self.filepath    = os.path.abspath(filepath)
+
+        # strip out local path
+        if (os.name == "nt"):
+            self.filename = self.filepath.split("\\")[-1]
+        else:
+            self.filename = self.filepath.split("/")[-1]
+
         self.dest_folder = dest_Drive_folder
-
-        # These are the supported MIME types for this application
-        self.mime_lookup = {
-            "pdf": "application/pdf",
-            "psd": "image/vnd.adobe.photoshop",
-            "jpg": "image/jpeg",
-            "png": "image/png"
-        }
-
-        file_ext = self.filename.split(".")[1]
-        try:
-            self.mime_type = self.mime_lookup[file_ext]
-        except KeyError as e:
-            print("\n\033[0;31mI'm sorry, it looks like I haven't accounted for that kind of file yet.\033[0m")
-            print("Please let me know if you see this.\n")
-            exit()              # application cannot continue without MIME type
+        self.mime_type = mimetypes.guess_type(self.filename)[0]
 
         print("\nAuthenticating...")
         self.creds   = self.authenticate()
 
         print("Locating file (in given folder, if specified)...")
         self.service = build("drive", "v3", credentials=self.creds)
-
-        self.known_file_IDs   = json.load(open("file_IDs.json", "r"))
-        self.known_folder_IDs = json.load(open("folder_IDs.json", "r"))
 
         if (dest_Drive_folder and dest_Drive_folder != ""):
             self.folder_ID = self.find_folder_ID()
@@ -88,9 +78,6 @@ class FileToUpload:
     # searches specified folder, if given
     def find_file_ID(self):
         
-        # if (self.filename in self.known_file_IDs.keys()):
-        #     return self.known_file_IDs[self.filename]
-        
         # build the query, depending on whether folder was specified/found
         query = "name = '" + self.filename + "'"
         if (self.folder_ID):
@@ -117,37 +104,21 @@ class FileToUpload:
             if page_token is None:
                 break
                 
+
         # if no matches, simply return None
         if not files:
             return None
         
-        # We shouldn't count trashed files, and update JSONs/dicts accordingly
-        if bool(files[0].get("trashed")):
-            self.known_file_IDs.pop(self.filename)
-
-            with open("files_IDs.json", "w") as f:
-                json.dump(self.known_file_IDs, f, indent=4)
-                
+        # if the file has been trashed, no ID (we can upload again)
+        elif (bool(files[0].get("trashed"))):  
             return None
-
-        # If multiple matches, take ID of most recently modified file
-        files_dict = {files[0]['name']: files[0]['id']}
-        self.known_file_IDs.update(files_dict)
-
-        # update file for read-in later
-        with open("files_IDs.json", "w") as f:
-            json.dump(self.known_file_IDs, f, indent=4)
-
-        if (self.filename in self.known_file_IDs.keys()):
-            return self.known_file_IDs[self.filename]
+        
         else:
-            return None
+            # If multiple matches, take ID of most recently modified file
+            return files[0]['id']
     
 
     def find_folder_ID(self):
-
-        # if (self.dest_folder in self.known_folder_IDs.keys()):
-        #     return self.known_folder_IDs[self.dest_folder]
         
         folders = []
         page_token = None
@@ -174,27 +145,18 @@ class FileToUpload:
         # if no matches, return None
         if not folders:
             return None
-
-        # if multiple matches, take the most recent modified
-        folders_dict = {folders[0]['name']: folders[0]['id']}
-        self.known_folder_IDs.update(folders_dict)
-
-        # update file for read-in later
-        with open("folder_IDs.json", "w") as f:
-            json.dump(self.known_folder_IDs, f, indent=4)
-
-        if (self.dest_folder in self.known_folder_IDs.keys()):
-            return self.known_folder_IDs[self.dest_folder]
         else:
-            return None
+            # if multiple matches, take the most recent modified
+            return folders[0]['id']
 
 
     def create_file(self):
+
         file_metadata = {"name": self.filename}
         if self.folder_ID:
             file_metadata.update({"parents": [self.folder_ID]})
-        
-        media = MediaFileUpload(self.filename, 
+    
+        media = MediaFileUpload(self.filepath, 
                                 mimetype=self.mime_type)
 
         # Call the Drive v3 API
@@ -204,15 +166,9 @@ class FileToUpload:
             .execute()
         )
 
-        new_file_ID = file.get("id")
-        self.known_file_IDs.update({self.filename: new_file_ID})
-
-        with open("file_IDs.json", "w") as f:
-            json.dump(self.known_file_IDs, f, indent=4)
-
 
     def update_file(self):
-        media = MediaFileUpload(self.filename, mimetype=self.mime_type)
+        media = MediaFileUpload(self.filepath, mimetype=self.mime_type)
 
         self.service.files().update(fileId=self.file_ID, 
                                     media_body=media
@@ -229,9 +185,10 @@ def main():
         dest_folder = sys.argv[2]
 
     try:
-        file = FileToUpload(filename=sys.argv[1], dest_Drive_folder=dest_folder)
+        file = FileToUpload(filepath=sys.argv[1], dest_Drive_folder=dest_folder)
     except FileNotFoundError:
-        print("A file was not specified. One is needed in order to run this program.")
+        print("\033[0;31mA file was not specified. One is needed to run this program.\003[0m")
+        print("Exiting...")
         exit(1)
 
     if (file.file_ID):      # if the file is already on Drive...
