d5f2dfbdce5bd731152d3d82817f85a7d37bd13b
Martin Reid <lmreid1997@gmail.com>
2024-11-16 21:08:02 -0700

    use absolute path to drive, compat with relative paths

--- utils/config_ext_drive.py
+++ utils/config_ext_drive.py
@@ -7,7 +7,7 @@ with open(os.path.abspath("./utils/external_drive_paths.json"), "r") as paths_fi
 print("\033[0;33m\nIt looks like you haven't selected an external drive yet.\033[0m")
 
 prompt  = """
-Please enter the (absolute) filepath to your external hard drive,
+Please enter the filepath to your external hard drive,
 using %s to separate folder names.
 (this path will be saved for latter, so you won't have to enter it again.)
 
@@ -29,10 +29,10 @@ while(not filepath_is_valid):
     # if it's a Windows operating system
     if (os.name == "nt"):
         path = input(prompt % dos_like)
-        drive_paths["DOSFilePath"] = path
+        drive_paths["DOSFilePath"] = os.path.abspath(path)
     else:
         path = input(prompt % unix_like)
-        drive_paths["UnixFilePath"] = path
+        drive_paths["UnixFilePath"] = os.path.abspath(path)
     
     filepath_is_valid = os.path.exists(path)
 
